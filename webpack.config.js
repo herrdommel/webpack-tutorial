b1002ffcae372e02aa8e9a66e107e1f881d2fb02
Dominik Mohr <herrdommel@gmail.com>
2024-04-12 11:18:28 +0200

    https://webpack.js.org/guides/caching/

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        compress: true,
-        hot: true,
-        port: 9000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Code Splitting',
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+module.exports = async (env, options) => {
+    const dev = options.mode === "development";
+    return {
+        entry: {
+            index: './src/index.js',
+        },
+        devtool: dev ? 'inline-source-map' : 'source-map',
+        devServer: {
+            static: './dist',
+            compress: true,
+            hot: true,
+            port: 9000,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Caching',
+            }),
+        ],
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        optimization: {
+            moduleIds: 'deterministic',
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
+            },
+        },
+    };
 };
